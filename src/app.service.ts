54e0fe3bc18b72eef4dffab66a5ea5c5a2db5232
Gustavo Valle <gustavo.vallerp@hotmail.com>
2023-04-13 23:11:49 -0300

    feat: add a timeout on service to get a more meanfull latency report

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  async getHello(): Promise<string> {
+    await new Promise((r) => setTimeout(r, 2580));
     return 'Hello World!';
   }
 }
